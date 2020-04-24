cfd37d92a878bcffc63d538678352515614a4e75
Maciek <maciekgrzela45@gmail.com>
2020-04-24 14:25:15 +0200

    [Strona Główna]: Dodano slider'y oraz footer

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        nav: false,
+        responsiveClass:true,
+        autoplay: true,
+        dots: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+});
